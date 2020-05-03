549bc801062d878d481fa9e629ba8e6d3b76c483
konopek1 <michal.konopka01@student.wat.edu.pl>
2020-05-03 22:14:16 +0200

    remove line break, decrease offset

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window.onload = () => {
   AOS.init({
-    offset: 120, // offset (in px) from the original trigger point
+    offset: 100, // offset (in px) from the original trigger point
     delay: 0, // values from 0 to 3000, with step 50ms
     duration: 1000,
     once:true,
@@ -11,7 +11,6 @@ window.onload = () => {
 function extendConsulting() {
   const consultingExtendContainer =  document.querySelector("#consultingExtendContainer");
   consultingExtendContainer.classList.toggle("hide");
-  window.scroll(0,pageYOffset + 200);
 }
 
 function scrollToSection(targetClass) {
